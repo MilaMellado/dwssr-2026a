68a3f1b18e48ad7b07405df9079be85fb1f26d06
MilaMellado <camilaam0908@outlook.com>
2026-05-11 15:56:56 +0000

    ✨feat: Winston Logger Update logging to use Winston in app.js and www.js, and fix clean:logs script path

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,21 +4,22 @@
  * Module dependencies.
  */
 
-import app from '../app.js';
-import createDebug from 'debug'; 
-import http from 'node:http';
+import app from "../app.js";
+import createDebug from "debug";
+import http from "node:http";
+// Importando el logger de Winston
+import logger from "../lib/winston.js";
 
-const debug = createDebug('dwssr:server');
-const info = createDebug('dwssr:info');
+const debug = createDebug("dwssr:server");
+const info = createDebug("dwssr:info");
 
 /**
  * Get port from environment and store in Express.
  */
-info('🚀 Starting server...');
-info('🔧 Configuring server...');
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
+logger.info(`🚀 Starting server... ${port}`);
+logger.info("🔧 Configuring server...");
+var port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -31,8 +32,8 @@ var server = http.createServer(app);
  */
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -59,22 +60,20 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      logger.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      logger.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -88,9 +87,7 @@ function onError(error) {
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  
-  debug('Listening on ' + bind);
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+
+  logger.info(`📢 App Listening on ${bind} port`);
 }
